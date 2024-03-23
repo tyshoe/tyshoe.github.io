26bed3bf6b6916e249a670b9c2faf9362cdad65e
Tyler Shoemake <tyshoe2010@gmail.com>
2024-03-23 00:11:39 -0500

    add themes to view containers, alter ul hover css, add script.js

---
+++ scripts.js
@@ -0,0 +1,22 @@
+const themeSelect = document.getElementById("theme-select")
+  ? document.getElementById("theme-select")
+  : null;
+const currentTheme = localStorage.getItem("theme")
+  ? localStorage.getItem("theme")
+  : null;
+
+// Set Dropdown to selected theme
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+  if (themeSelect){themeSelect.value = currentTheme;}
+}
+
+// If theme changes, store in local storage
+if (themeSelect) {
+  themeSelect.addEventListener("change", function () {
+    const selectedTheme = themeSelect.value;
+    document.documentElement.setAttribute("data-theme", selectedTheme);
+    localStorage.setItem("theme", selectedTheme);
+    console.log(themeSelect.value);
+  });
+}
